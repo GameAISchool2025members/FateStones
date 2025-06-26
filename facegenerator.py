55606aea9e4c4af727336088929e7ce0df03e2ca
aaronchapin-tocaboca <aaron.chapin@tocaboca.com>
2025-06-26 17:16:38 +0200

    STLs for each die with a middling average

--- facegenerator.py
+++ facegenerator.py
@@ -1,12 +1,12 @@
 import sys
 
-def printDice(filepath):
-    with open(filepath) as facesFile:
+def printDice(inputFilePath, outputFilePath):
+    with open(inputFilePath) as facesFile:
         for line in facesFile:
             filename = line.strip().replace(" ", "")
             faceValues = line.split(' ')
             stl = printDie(faceValues)
-            with open("generated/"+filename+".stl", "w") as stlfile:
+            with open(outputFilePath+"/"+filename+".stl", "w") as stlfile:
                 stlfile.write(stl)
 
 def printDie(faceValues):
@@ -14,7 +14,7 @@ def printDie(faceValues):
     out = "solid Cheetah3Dfile\n"
     faceIndex = 0
     for face in folders:
-        pips = faceValues[faceIndex]
+        pips = faceValues[faceIndex].strip()
         with open(face+"/"+pips+".stl") as faceFile:
             for line in faceFile:
                 if "solid" not in line:
@@ -26,4 +26,6 @@ def printDie(faceValues):
 if(len(sys.argv) == 7):
     print(printDie(sys.argv[1:]))
 elif(len(sys.argv) == 2):
-    printDice(sys.argv[1])
+    printDice(sys.argv[1],"generated")
+elif(len(sys.argv) == 3):
+    printDice(sys.argv[1], sys.argv[2])
